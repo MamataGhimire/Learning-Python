8e02b3018e0d801aeeaf6a6c46fb337cf1865ad2
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2025-02-01 20:47:38 +0545

    Add files via upload
    Data type and Input function

---
+++ day2.py
@@ -0,0 +1,36 @@
+# type of data...........
+# a=1
+# name="harry"
+# b="100"
+# t=type(a) #<class 'int'>
+# u=type(name)
+# v=int(b)#converting string into int ,we also have str(),float()
+
+# print(t,u,type(v))
+
+# input function........
+
+# to add two number...........
+# a=int(input("enter your first number"))
+# b=int(input("enter your second number"))
+# sum=a+b
+# print("the sum of your numbers is :",sum)
+
+# to give remainder..........
+# a=17
+# b=5
+# print("the remainder is ", a%b)
+
+# to check a is greater then b or not......
+# ans=a>b
+# print(ans)
+
+# to find the average of two number.....
+# a=int(input("enter your first number"))
+# b=int(input("enter your second number"))
+# avg=(a+b)/2
+# print("average of your two number is ",avg)
+
+# to find the square.....
+# a=int(input("enter your  number"))
+# print("square of your number is ",a**2) #** for power ^ not valid
