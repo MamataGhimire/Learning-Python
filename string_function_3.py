b253caeb34870fb622c508ad6eff1a12eeb28513
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2025-02-04 13:52:04 +0545

    Add files via upload

---
+++ string_function_3.py
@@ -0,0 +1,8 @@
+name="salina"
+size=len(name) # give the size of string
+print("size of name ",size)
+print("does it end with 'na' ",name.endswith("na"))#endswith() check the last element of string
+print("does it start with 'sa' ",name.startswith("sa"))#endswith() check the first element of string
+print(name.capitalize())#it capitalize only first letter
+print(name.find("l"))#find the letter and print the index
+print(name.replace("a","e"))#replae the letter
