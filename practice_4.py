27849a61af4a91ae4bc80df99b17aef36a93da38
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2025-02-05 20:01:20 +0545

    Add files via upload

---
+++ practice_4.py
@@ -0,0 +1,14 @@
+# # 1....
+# name=input("enter your name")
+# print(f"Good Afternoon! {name}")
+
+# # 2....
+# letter='''Dear <|name|>
+# you are selected
+# <|Date|>'''
+# print(letter.replace("<|name|>",name).replace("<|Date|>","24 sept"))
+
+#3 double space detect.....
+sen="Is there double  sapce"
+print(sen.find("  "))
+print(sen.replace("  "," "))
