758a9a0d61e34b666c3fe33ebab96f35ce5e5985
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2025-02-06 16:10:49 +0545

    Add files via upload

---
+++ List_5.py
@@ -0,0 +1,26 @@
+friends=["apple","orange",5,2.053,"manoj"]
+# print(friends[0])
+# friends[0]="grapes" #unlike string lists are mutable
+# print(friends[0])
+# print(friends[1:4])
+
+#methods of list
+# friends.append("sradha") #append() insert the element in last of list
+# print(friends)
+
+# friends.insert(4,"kashis") #insert kashis in the 4  index
+# print(friends)
+
+# friends.remove("apple")# remove the element
+# print(friends)
+
+num=[3,9,5,1,2]
+# num.sort() # sort() arrange list in assending order
+# print(num)
+
+# num.reverse() # reverse() reverse the element in list
+# print(num)
+
+# print(num.pop(3))#print the value of element which is poped/returend value
+# print(num)#remove th ethird index element
+
