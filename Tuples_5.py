663be2301fd98a6ab9aa1048a1ca0eb4f3787be4
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2025-02-07 22:46:35 +0545

    Add files via upload

---
+++ Tuples_5.py
@@ -0,0 +1,18 @@
+#tuple is immutable
+a=(1,3,7,False,7,"Rohan")
+#when we have to make tuples with one element
+# b=(1,)#b=(1) py will read it as int 
+# print(type(a))
+
+#method of tuples
+
+# no=a.count(7) # count() give the how many time the value is repeat in tuples
+# print(no)
+
+# i=a.index(7)# give the index of first occurance
+# print(i)
+
+# print(len(a)) #len() give the length of tuples
+
+# c,d,e,f,g,h = a # assing the value by the elements in tuples respectively
+# print(c,d,e,f,g,h)
