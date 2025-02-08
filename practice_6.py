a2d6ed2a8271efad20d6f62f1ff8a53415bc588c
Mamata Ghimire <89641735+MamataGhimire@users.noreply.github.com>
2025-02-08 16:52:03 +0545

    Add files via upload

---
+++ practice_6.py
@@ -0,0 +1,34 @@
+#WAP to store 7 fruits in a list entered by the user
+
+# fruits=[]
+# i=1
+# for i in range(7):
+#   name=input("Enter name of seven fruits")
+#   fruits.append(name)
+#   i=i+1
+# print(fruits)
+
+# #WAP to accept marks of 6 student and display them in sorted manner
+
+# marks=[]
+# i=1
+# for i in range(6):
+#   mark=int(input("Enter the marks of student"))
+#   marks.append(mark)
+#   i=i+1
+#   marks.sort()
+# print(marks)
+
+#check that tuple type cannot be changed in tuples
+# a=(34, 456,"hari")
+# a[2]="gari"
+# print(a)
+
+# WAP to sum of list with four number
+# num=[1,2,3,4]
+# print(sum(num))
+
+#Count zero in following tuple
+num=(7,0,8,0,0,9)
+print(num.count(0))
+
